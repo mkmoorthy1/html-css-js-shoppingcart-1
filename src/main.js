d13a9a175fd1c94d5e0ffd3d7f8b1a4ca13b62c7
mkmoorthy1 <mkmoorthy1@gmail.com>
2022-07-26 18:51:22 +0530

    Final Prj V1 Ready as on 26-07-2022 with small change in css file

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ let generateShop = () => {
 			let search = basket.find(x => x.id === id) || [];
 			return `<div id=product-id-${id} class="item">
 		<img width="220" src=${img} alt="" srcset="">
-		<div class="details">
+		<div class="shop-details">
         <h3>${name}</h3>
         <p>${desc}.</p>
         <div class="price-quantity">
